9a09baa7989fd68398c1f759527c0a921c12a598
Advait <76993204+777advait@users.noreply.github.com>
2024-10-29 21:15:18 +0530

    update build settings

--- next.config.js
+++ next.config.js
@@ -5,6 +5,19 @@
 await import("./src/env.js");
 
 /** @type {import("next").NextConfig} */
-const config = {};
+const config = {
+  typescript: {
+    /**
+     * Disable type checking since eslint handles this for us.
+     */
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    /**
+     * Warnings are shown by default in the console. Errors are not.
+     */
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default config;
